4de6786f2d338297e221a7efb82929df5ba3dbd9
ThiagoBD <thiagobarrosdias@gmail.com>
2024-06-27 13:28:27 -0300

    Start project

---
+++ src/main/java/com/thiagobarrosdias/doc_converter/DocConverterApplication.java
@@ -0,0 +1,13 @@
+package com.thiagobarrosdias.doc_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagobarrosdias/doc_converter/DocConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagobarrosdias.doc_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
